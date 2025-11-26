ff9e2b3dcaf43f6003926ea397208ded4398cc46
盛辉 <hui.sheng@sunmi.com>
2025-11-26 16:01:14 +0800

    Update install.sh

--- internal/grpc/install.sh
+++ internal/grpc/install.sh
@@ -8,10 +8,10 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 # gateway
 go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 
 # buf
 go install github.com/bufbuild/buf/cmd/buf@latest
 
 # swagger
 go install github.com/go-swagger/go-swagger/cmd/swagger@latest
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
